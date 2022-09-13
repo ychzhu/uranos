d3d5a2dc3d2b616122230beeca5b4ea3f01075b1
Martin Schrön <martin.schroen@ufz.de>
2022-09-13 19:15:26 +0200

    Updated figure

--- doc/scripts/make_uranos_pubs.py
+++ doc/scripts/make_uranos_pubs.py
@@ -4,6 +4,7 @@
 
 # %%
 from publib import PubList
+from datetime import date
 
 # %%
 P = PubList()
@@ -15,6 +16,10 @@ content = """
 
 Citations: **{cites:.0f}** (based on [CrossRef.org](https://www.crossref.org/))
 
+*Figure. Left: Publications and their total citations (bar width). Right: Cumulative publications over the years. (Spiral package credits: [G. Skok, 2022](https://doi.org/10.3390/app12136609))*
+![Publications and citations per year](pubplot.png)
+
+## Details 
 {publist}
 """.format(
     num     = P.total_pubs,
@@ -28,53 +33,43 @@ with open('../PUBLICATIONS.md', 'w', encoding="utf-8") as fh:
     fh.write(content)
     
 # %%
-from pandas import DataFrame
-history = DataFrame([dict(
-    year = today.year,
-    pubs = P.total_pubs)])
-
-# %%
-plotdata = P.data[['year','authors','cites']]
+from pandas import DataFrame, to_datetime
+plotdata = P.data[['year','date','authors','cites']]
 plotdata['firstauthor'] = [x[0] for x in plotdata.authors]
 plotdata['label'] = ['{1}\n{0} et al.'.format(x[0], y) for x,y in zip(plotdata.authors, plotdata.year)]
 plotdata = plotdata[::-1]
+plotdata['t'] = to_datetime(plotdata.date)
+plotdata['num'] = 1
+plotdata['num'] = np.cumsum(plotdata['num'])
 plotdata
 
 # %%
-from spiral_strip_library_v04 import *
-duration = [ 7,  5,  9, 23,  6,  6,  4]
-deaths = [ 85., 28., 22.5, 9.846, 7., 3.95, 3. ]
-
-color1='#e82b33'
-color2='#31aae0'
-segment_color=[]
-for il in range(len(duration)):
-	segment_color.append(color1)
-	segment_color.append(color2)
-
-segment_color='C0'
-
-width = []
-segment_length = 150
-
-draw_spiral_strip(image_filename="example06.pdf", r0=0, space=0, fi0_deg=0,
-    number_of_segments = len(plotdata), width=plotdata.cites.values+1, segment_length=segment_length,
-    maximum_length_of_subsegment=10, segment_color = segment_color,
-    values=None, colormap_name=None,
-    labels1_text=plotdata.label.values, labels1_fontsize=5, labels1_color="dimgray", labels1_pad=35,
-    antialiased = True, gap_between_consecutive_segments = 15,
-    dpi=300)
-
-# %%
 from corny.figures import Figure
-c_pubs = 'C0'
-c_cite = '#000000'
-with Figure(size=(8,3), save='out/pubs_per_year.png') as ax:
-    ax.bar(history[:-1].year, history[:-1].pubs, color=c_pubs, alpha=0.5)
-    ax.bar(history[-1:].year, history[-1:].pubs, color=c_pubs, alpha=0.5, hatch='//', edgecolor='white')
-    #ax.scatter(today.year, spy_now.pubs.values[-1] + PP.total_pubs, color='#BBBBBB')
-    ax.set_ylabel('Publications')
-    ax.spines['left'].set_color(c_pubs)
-    ax.spines['top'].set_color('w')
-    ax.yaxis.label.set_color(c_pubs)
-    ax.tick_params(axis='y', colors=c_pubs)
+from spiral_strip_library_v04 import *
+segment_color = 'C0'
+segment_length = 130
+
+with Figure(layout=(1,2), size=(8,4), save='../pubplot.png') as axes:
+    ax = axes[0]
+    draw_spiral_strip(image_filename="example06.pdf", r0=0, space=0, fi0_deg=110,
+        number_of_segments = len(plotdata), width=plotdata.cites.values+1, segment_length=segment_length,
+        maximum_length_of_subsegment=10, segment_color = segment_color,
+        values=None, colormap_name=None,
+        labels1_text=plotdata.label.values, labels1_fontsize=5, labels1_color="dimgray", labels1_pad=25,
+        antialiased = True, gap_between_consecutive_segments = 10,
+        ax=ax)
+    ax.autoscale(enable=True, axis='x', tight=True)
+    ax = axes[1]
+    ax.set_title('Publications (total)', fontsize=7)
+    ax.plot(plotdata.t, plotdata.num, drawstyle='steps-post')
+    ax.set_xlim(date(2015,1,1), date(2022,12,31))
+    ax.set_yticks(np.arange(0,len(plotdata),5))
+    ax.xaxis.set_tick_params(labelsize=6)
+    ax.yaxis.set_tick_params(labelsize=6)
+    ax.grid(color='#BBBBBB', ls=':', axis='y', alpha=1)
+    ax.spines.top.set(visible=False)
+    ax.spines.left.set(visible=False)
+    ax.spines.right.set(visible=False)
+    ax.tick_params(colors='#BBBBBB', which='both', axis='y', tick1On=False)
+    ax.spines["bottom"].set_position(("data", 0))
+    ax.plot(1, 0, ">k", transform=ax.get_yaxis_transform(), markersize=3, clip_on=False)

ffbb5539f575fa50b9d0baccc3770228b79b0fb7
Martin Schrön <martin.schroen@ufz.de>
2022-09-14 14:58:11 +0200

    Finetuned color themes and cleaned code.

--- doc/scripts/make_uranos_pubs.py
+++ doc/scripts/make_uranos_pubs.py
@@ -53,35 +53,52 @@ plotdata['num'] = np.cumsum(plotdata['num'])
 plotdata
 
 # %%
-from corny.figures import Figure
-from spiral_strip_library_v04 import *
-segment_color = 'C0'
-segment_length = 130
-
 import matplotlib.pyplot as plt
-#plt.style.use('default')
-plt.style.use('dark_background')
-with Figure(layout=(1,2), size=(8,4), save='../pubplot-dark.png') as axes:
-    ax = axes[0]
-    draw_spiral_strip(r0=0, space=0, fi0_deg=110,
-        number_of_segments = len(plotdata), width=plotdata.cites.values+1, segment_length=segment_length,
-        maximum_length_of_subsegment=10, segment_color = segment_color,
-        values=None, colormap_name=None, labels1_color='C7',
-        labels1_text=plotdata.label.values, labels1_fontsize=5, labels1_pad=25,
-        antialiased = True, gap_between_consecutive_segments = 10,
-        ax=ax)
-    ax.autoscale(enable=True, axis='x', tight=True)
-    ax = axes[1]
-    ax.set_title('Publications (total)', fontsize=7)
-    ax.plot(plotdata.t, plotdata.num, drawstyle='steps-post')
-    ax.set_xlim(date(2015,1,1), date(2022,12,31))
-    ax.set_yticks(np.arange(0,len(plotdata),5))
-    ax.xaxis.set_tick_params(labelsize=6)
-    ax.yaxis.set_tick_params(labelsize=6)
-    ax.grid(color='#BBBBBB', ls=':', axis='y', alpha=1)
-    ax.spines.top.set(visible=False)
-    ax.spines.left.set(visible=False)
-    ax.spines.right.set(visible=False)
-    ax.tick_params(colors='#BBBBBB', which='both', axis='y', tick1On=False)
-    ax.spines["bottom"].set_position(("data", 0))
-    ax.plot(1, 0, ">k", transform=ax.get_yaxis_transform(), markersize=3, clip_on=False)
+# Corny package from: https://git.ufz.de/CRNS/cornish_pasdy
+from corny.figures import Figure
+# Sprial package from: https://github.com/skokg/Spiral-strip
+from spiral_strip_library_v04 import *
+
+radius          = 0
+space           = 0
+rotation        = 110 # try to keep the latest paper on top
+segment_len     = 130
+segment_len_max = 10
+segment_gap     = 10
+label_padding   = 25
+label_fontsize  = 5
+
+themes = dict(
+    dark  = dict(style='default',         nametag='-light', fgcolor='black'),
+    light = dict(style='dark_background', nametag='-dark',  fgcolor='white'))
+
+for theme in themes:
+    plt.style.use(themes[theme]['style'])
+    with Figure(layout=(1,2), size=(8,4),
+        save='../pubplot%s.png' % themes[theme]['nametag']) as axes:
+        
+        ax = axes[0]
+        draw_spiral_strip(r0=radius, space=space, fi0_deg=rotation,
+            number_of_segments=len(plotdata), width=plotdata.cites.values+1,
+            segment_length=segment_len, maximum_length_of_subsegment=segment_len_max,
+            segment_color='C0', values=None, colormap_name=None, labels1_color='#BBBBBB',
+            labels1_text=plotdata.label.values, labels1_fontsize=label_fontsize,
+            labels1_pad=label_padding, antialiased=True,
+            gap_between_consecutive_segments = segment_gap,
+            ax=ax)
+        ax.autoscale(enable=True, axis='x', tight=True)
+        
+        ax = axes[1]
+        ax.set_title('Publications (total)', fontsize=7)
+        ax.plot(plotdata.t, plotdata.num, drawstyle='steps-post')
+        ax.set_xlim(date(2015,1,1), date(2022,12,31))
+        ax.set_yticks(np.arange(0,len(plotdata),5))
+        ax.xaxis.set_tick_params(labelsize=6)
+        ax.yaxis.set_tick_params(labelsize=6)
+        ax.grid(color='#BBBBBB', ls=':', axis='y', alpha=1)
+        ax.spines.top.set(visible=False)
+        ax.spines.left.set(visible=False)
+        ax.spines.right.set(visible=False)
+        ax.tick_params(colors='#BBBBBB', which='both', axis='y', tick1On=False)
+        ax.spines["bottom"].set_position(("data", 0))
+        ax.plot(1, 0, ">", color=themes[theme]['fgcolor'], transform=ax.get_yaxis_transform(), markersize=3, clip_on=False)
